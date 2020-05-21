1aaba291ae1fe69a44c258b078508c632f8fb4f7
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-05-21 21:53:28 +0200

    feat: add Wiki to views

---
+++ src/views/Wiki.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 class="title">What's the Coronavirus?</h1>
+    <p>
+      <a href="https://en.wikipedia.org/wiki/Coronavirus_disease_2019">Coronavirus</a> disease 2019 (COVID-19) is an <a href="https://en.wikipedia.org/wiki/Infection">infectious disease</a> caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2).
+      It was first identified in December 2019 in <a href="https://en.wikipedia.org/wiki/Wuhan">Wuhan</a>, <a href="https://en.wikipedia.org/wiki/Central_China">China</a>, and has since spread globally, resulting in an <a href="https://en.wikipedia.org/wiki/COVID-19_pandemic">ongoing pandemic</a>.
+    </p>
+
+    <h1 class="title">What're the symptoms?</h1>
+    <p>
+      Common symptoms include fever, cough, <a href="https://en.wikipedia.org/wiki/Fatigue">fatigue</a>, <a href="https://en.wikipedia.org/wiki/Shortness_of_breath">shortness of breath</a>, and <a href="https://en.wikipedia.org/wiki/Anosmia">loss of smell</a> and <a href="https://en.wikipedia.org/wiki/Ageusia">taste</a>.
+      Complications may include <a href="https://en.wikipedia.org/wiki/Pneumonia">pneumonia</a> and acute <a href="https://en.wikipedia.org/wiki/Acute_respiratory_distress_syndrome">respiratory distress syndrome</a>.
+      The time from exposure to onset of symptoms is typically around five days but may range from two to fourteen days.
+      There is no <a href="https://en.wikipedia.org/wiki/COVID-19_vaccine">known vaccine</a> or specific <a href="https://en.wikipedia.org/wiki/Antiviral_drug">antiviral treatment</a>. Primary treatment is symptomatic and supportive therapy.
+    </p>
+
+    <h1 class="title">How does it spread?</h1>
+    <p>
+      The virus is primarily <a href="https://en.wikipedia.org/wiki/Transmission_(medicine)">spread</a> between people during close contact, most often via <a href="https://en.wikipedia.org/wiki/Respiratory_droplet">small droplets</a> produced by coughing, sneezing, and talking.
+      The droplets usually fall to the ground or onto surfaces rather than <a href="https://en.wikipedia.org/wiki/Airborne_disease">travelling through air</a> over long distances. Less commonly, people may become infected by touching a contaminated surface and then touching their face.
+      It is most contagious during the first three days after the onset of symptoms, although spread is possible before symptoms appear, and from people who do not show symptoms.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Wiki extends Vue {
+
+}
+</script>
