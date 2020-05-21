a8a5ffbbca75e7a15d4443a2cff3dd562adb5b8c
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-05-22 00:03:43 +0200

    feat: poll values from VuexStore

--- src/components/QuickNumbers.vue
+++ src/components/QuickNumbers.vue
@@ -13,6 +13,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import { Watch } from 'vue-property-decorator'
 
 interface QuickNumbersEntry {
   icon: string;
@@ -27,30 +28,47 @@ interface QuickNumbersEntry {
   }
 })
 export default class QuickNumbers extends Vue {
+  get payload (): object { return this.$store.state.data }
+  set payload (value: object) { this.$store.commit('SET_DATA', value) }
+
+  @Watch('payload', { immediate: true })
+  onPayloadChanged (newValue: any, oldValue: any): void {
+    if (newValue === undefined) return
+
+    this.numbers[1].value = newValue.Global.TotalDeaths
+    this.numbers[2].value = newValue.Global.TotalRecovered
+    this.numbers[3].value = newValue.Global.TotalConfirmed
+
+    this.numbers[0].value =
+      this.numbers[1].value +
+      this.numbers[2].value +
+      this.numbers[3].value
+  }
+
   numbers: QuickNumbersEntry[] = [
     {
       icon: 'mdi-shield-check',
       color: 'primary',
       description: 'Total',
-      value: 1456965
+      value: 0
     },
     {
       icon: 'mdi-skull',
       color: 'error',
       description: 'Deceased',
-      value: 63263
+      value: 0
     },
     {
       icon: 'mdi-virus',
       color: 'warning',
       description: 'Confirmed',
-      value: 1162857
+      value: 0
     },
     {
       icon: 'mdi-arm-flex',
       color: 'success',
       description: 'Recovered',
-      value: 230845
+      value: 0
     }
   ]
 }
