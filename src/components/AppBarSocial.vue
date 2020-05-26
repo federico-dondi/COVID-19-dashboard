06cd81aa1c5b28783f3f439ab7a63ee60a873d47
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-05-26 17:32:01 +0200

    feat: add AppBarSocial

---
+++ src/components/AppBarSocial.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-btn icon v-on="on"
+        target="_blank"
+        :href="href"
+        :hreflang="hreflang">
+        <v-icon>{{ icon }}</v-icon>
+      </v-btn>
+    </template>
+
+    <span>{{ caption }}</span>
+  </v-tooltip>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+@Component
+export default class AppBarSocial extends Vue {
+  @Prop({ type: String }) readonly icon!: string
+  @Prop({ type: String }) readonly caption!: string
+  @Prop({ type: String }) readonly href!: string
+  @Prop({ type: String }) readonly hreflang!: string
+}
+</script>
