e8668fa9079e30d43ad4e923c417bb2c2840905c
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-05-28 15:32:40 +0200

    fix: split Store int different fields

--- src/plugins/store.ts
+++ src/plugins/store.ts
@@ -3,15 +3,51 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-interface RootState { data: object | undefined }
+interface Global {
+  NewConfirmed: number;
+  TotalConfirmed: number;
+  NewDeaths: number;
+  TotalDeaths: number;
+  NewRecovered: number;
+  TotalRecovered: number;
+}
+
+interface Country {
+  Country: string;
+    CountryCode: string;
+    Slug: string;
+    NewConfirmed: number;
+    TotalConfirmed: number;
+    NewDeaths: number;
+    TotalDeaths: number;
+    NewRecovered: number;
+    TotalRecovered: number;
+    Date: Date;
+}
+
+interface RootState {
+  Global: Global;
+  Countries: Country[];
+}
 
 export default new Vuex.Store<RootState>({
   state: {
-    data: undefined
+    Global: {
+      NewConfirmed: 0,
+      TotalConfirmed: 0,
+      NewDeaths: 0,
+      TotalDeaths: 0,
+      NewRecovered: 0,
+      TotalRecovered: 0
+    },
+    Countries: []
   },
   mutations: {
-    SET_DATA (state, value: object | undefined) {
-      state.data = value
+    SET_GLOBAL (state, value: Global) {
+      state.Global = value
+    },
+    SET_COUNTRIES (state, value: Country[]) {
+      state.Countries = value
     }
   },
   actions: {
